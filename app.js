394baf85c0ccaa72007b391cc750439e8a999ef3
programadordelfuturo21 <luis.saravia@globals.one>
2023-08-12 18:00:15 -0500

    finish

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-const form = document.querySelector('#form');
+const email = document.querySelector(".email");
+const password = document.querySelector(".pass");
+const btn = document.querySelector(".btn");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+const post = async (email, pass) => {
+    return await fetch("http://www.luis.com/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        pass: pass,
+      }),
+    });
+  };
+  
 
-    console.log(e)
-    console.log(e.target.value)
-
-    fetch('')
-})
+btn.addEventListener("click", async () => {
+  const emailValue = email.value;
+  const passValue = password.value;
+  try {
+    const response = await post(emailValue, passValue);
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+    } else {
+      console.error("Error en la solicitud:", response.status, response.statusText);
+    }
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+  }
+});
